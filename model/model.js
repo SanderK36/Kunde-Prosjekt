05428772fa14a0a84fd3652e6365c619e6894b16
Jasminee95 <jasmine_745@hotmail.com>
2025-03-27 11:36:31 +0100

    Update model.js

---
+++ model/model.js
@@ -0,0 +1,42 @@
+
+const model = {
+    app:{
+        currentPage: 'homePage',
+
+        pages:['homePage', 'logInPage', 'userPage', 'searchPage', 'assignmentPage', 'feedbackPage', ],
+
+    },
+
+    input:{
+        logInInput: {emailInput: "", passwordInput: "", admin: true,},
+        
+        comment:{text:'', assignmentId:1, },
+
+        //Nice to have 
+        rating: {userId:0 , ratingNumber:5,},
+
+        search: {searchInput: ''},
+
+    },
+
+    data:{
+        login: false,
+        header:[
+            { id: 1, logo:'', assignments:'', search:'', notification:'', calender:'', user:''},       
+    ],
+        
+        week:'',
+        assignments: [
+            { id: 1, week:'', day:'',  subject: '', tasks: '', tags: [], isDelivered: false,}
+        ],
+
+        tags: [
+            {theme: '', tagName: '', color: ''}
+        ],
+
+        users: [
+            {id:1, email: '', name: '', password:'', }
+        ],
+        date:{day:1,week:1,month:1,year:2025}
+    },
+}
