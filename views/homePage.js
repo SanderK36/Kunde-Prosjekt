63f1aaffb530e23992656089c04a4f623fd33b20
Jasminee95 <jasmine_745@hotmail.com>
2025-04-01 12:17:19 +0200

    Update homePage.js

--- views/homePage.js
+++ views/homePage.js
@@ -8,12 +8,17 @@ function homePageView(){
     <div class ="logo">Logo</div>
     <button id="assignmentBtn">Mine oppgaver</button>
     <i class="fa-solid fa-calendar-days"></i>
-    <i class="fa-solid fa-magnifying-glass"></i>
     <i class="fa-solid fa-bell"></i>
     <div class="userButton">Bruker</div>
+
+    <div id="search">
+    <input type="text" id="searchInput" placeholder="Søk...">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+
     </div> <br>
 
-    <div id='currentWeek'>Uke 8</div>
+    <h1 id='currentWeek'>Uke 8</h1>
 
     <div class="test"></div>
     
@@ -22,20 +27,26 @@ function homePageView(){
     for(let i = 1; i <= 21; i++){
         let content = '';
 
-        if(i === 1) content = "Uke";
-        else if(i === 2) content = "Dag";
-        else if(i === 3) content = "Tema";
-        else if(i === 4) content = "Oppgave";
-        else if(i === 5) content = "Tags";
+        if(i === 1) content = `<h1>Uke</h1>`;
+        else if(i === 2) content = `<h1>Dag</h1>`;
+        else if(i === 3) content = `<h1>Tema</h1>`;
+        else if(i === 4) content = `<h1>Oppgave</h1>`;
+        else if(i === 5) content = `<h1>Tags</h1>`;
         else if(i === 6) content = "";
-        else if(i === 7) content = "Levert?";
+        else if(i === 7) content = `<h1>levert?</h1>`;
         else if(i === 8 || i === 15) content = "8";
         else if(i === 9) content = "Mandag";
         else if(i === 10) content = "Psykologisk trygghet"
         else if(i === 11) content = "Hvordan gjøre et godt morgenmøte";
-        else if(i === 13) content = `<div class="commentAndFile"><i class="fa-solid fa-file"></i> <i class="fa-solid fa-comment"></i></div>`;
+        else if(i === 12) content = "Morramøtet bli kjent";
+        else if(i === 13) content = `<div class="gridIcons"><i class="fa-solid fa-file"></i> <i class="fa-solid fa-comment"></i></div>`;
+        else if(i === 14) content = `<div class="gridIcons"><i class="fa-solid fa-square-check"></i></div>`;
         else if(i === 16) content = "Tirsdag";
-        else if(i === 20) content = `<div class="commentAndFile"><i class="fa-solid fa-file"></i> <i class="fa-solid fa-comment"></i></div>`;
+        else if(i === 17) content = "Psykologisk trygghet"
+        else if(i === 18) content = "Hvordan være forberedt"
+        else if(i === 19) content = "Signaturstyrker Kommunikasjon"
+        else if(i === 20) content = `<div class="gridIcons"><i class="fa-solid fa-file"></i> <i class="fa-solid fa-comment"></i></div>`;
+        else if(i === 21) content = `<div class="gridIcons"><i class="fa-solid fa-square-xmark"></i></div>`;
         else content = `Item ${i}`;
 
         test.innerHTML += `<div class ="box">${content}</div>`;
