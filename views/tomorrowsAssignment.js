d190e795904592755b279a88b0a53745ee30c9e3
Jasminee95 <jasmine_745@hotmail.com>
2025-04-02 13:58:01 +0200

    Create tomorrowsAssignment.js

---
+++ views/tomorrowsAssignment.js
@@ -0,0 +1,20 @@
+function tomorrowsAssignmentView(){
+    document.getElementById('app').innerHTML = /*HTML*/ `
+    
+    <div id='header'>
+    <div class ="logo">Logo</div>
+    <button id="assignmentBtn">Mine oppgaver</button>
+    <i class="fa-solid fa-calendar-days"></i>
+    <i class="fa-solid fa-bell" onclick="tomorrowsAssignmentView()"></i>
+    <div class="userButton" onclick="userPageView()">Bruker</div>
+
+    <div id="search">
+    <input type="text" id="searchInput" placeholder="Søk...">
+    <i class="fa-solid fa-magnifying-glass" onclick="searchAssignment()"></i>
+    </div>
+
+    </div> <br>
+
+    
+    `;
+}
