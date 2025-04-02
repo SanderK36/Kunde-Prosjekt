bb4491a6c8e720db9e1b36f8f4f1df7945b69663
Jasminee95 <jasmine_745@hotmail.com>
2025-04-02 13:57:47 +0200

    Update userPage.js

--- views/userPage.js
+++ views/userPage.js
@@ -7,12 +7,12 @@ function userPageView(){
     <div class ="logo">Logo</div>
     <button id="assignmentBtn" onclick="homePageView()">Mine oppgaver</button>
     <i class="fa-solid fa-calendar-days"></i>
-    <i class="fa-solid fa-bell"></i>
+    <i class="fa-solid fa-bell" onclick="tomorrowsAssignmentView()></i>
     <div class="userButton">Bruker</div>
 
     <div id="search">
     <input type="text" id="searchInput" placeholder="Søk...">
-    <i class="fa-solid fa-magnifying-glass"></i>
+    <i class="fa-solid fa-magnifying-glass" onclick="searchAssignment()"></i>
     </div>
 
     </div> <br>
