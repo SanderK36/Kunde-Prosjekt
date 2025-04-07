05ef1be19df937b4965dd6d91dd6f7407d5f670b
Jasminee95 <jasmine_745@hotmail.com>
2025-04-07 14:32:55 +0200

    07.04.25

---
+++ views/adminClassPage.js
@@ -0,0 +1,19 @@
+
+function adminClassPageView(){
+    document.getElementById('app').innerHTML = /*HTML*/ `
+    
+    <div id='header'>
+    <div class ="logo">Logo</div>
+    <button id="assignmentBtn" onclick="homePageView()">Mine oppgaver</button>
+    <i class="fa-solid fa-calendar-days" onclick="calendarView()"></i>
+    <i class="fa-solid fa-bell" onclick="tomorrowsAssignmentView()"></i>
+    <div class="userButton" onclick="userPageView()">Bruker</div>
+
+    <div id="search">
+    <input type="text" id="searchInput" placeholder="Søk...">
+    <i class="fa-solid fa-magnifying-glass" onclick="searchAssignment()"></i>
+    </div>
+`;
+
+
+}
